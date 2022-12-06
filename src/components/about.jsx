a8aab4b7e34b0e7f9fa2aea6ef676a54c2c3165c
danielvn7 <vnc.dreamer@gmail.com>
2022-12-06 20:26:04 -0300

    Revert "Atualizado"
    This reverts commit 83959b506446285183f4bd9d9bc57408a07f247d.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,11 +13,9 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">Sobre mim</span>
                     <h2 className="colorlib-heading">Quem eu sou?</h2>
-                    <p>Após anos trabalhando no ramo administrativo onde agreguei muita experiência, decidi mudar e agora estou em transição de carreira para a área de Desenvolvedor de Software. 
-Sou estudante de Análise e Desenvolvimento de Sistemas, graduado em Logística. 
-<p></p>Em busca de adquirir mais experiência no mercado, avançar nos estudos e profissionalmente.
-Sou um entusiasta em tecnologia, inovação e estou sempre disposto a aprender coisas novas. 
-
+                    <p>Entusiasta na área de desenvolvimento atuando com a stack JavaScript, ReactJS.
+Atuando como front-end Júnior.
+Em busca de novos desafios , sempre buscando ir além do esperado.
                 </p>
                     </div>
                 </div>
